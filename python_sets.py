b308501e42b0d749897693d8aca946600edcc24c
Hlomohang Sethuntsa <hlomohangsethuntsa3@gmail.com>
2025-07-12 16:24:23 +0200

    computational thinking programs
    Computational Thinking programs

---
+++ python_sets.py
@@ -0,0 +1,16 @@
+# Define two sets
+A = {1,2,3,4}
+B = {3,4,5,6}
+
+# Set operations in action
+union_set = A.union(B)
+
+intersection_set = A.intersection(B)
+
+difference_set = A.difference(B)
+
+print("set A: ", A)
+print("set B: ", B)
+print("Union: ", union_set)
+print("intersection: ", intersection_set)
+print("Difference (A - B): ", difference_set)
